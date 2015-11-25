644e17431c30fa15a5d68bcae2c1c82aaddaeccc
Deon Hua <deonhbci@gmail.com>
2015-11-25 01:56:04 -0500

    Harmonize designs, remove unused code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,52 @@
-module.exports = function(grunt) {
-
-    grunt.initConfig({
-        watch: {
-            sass:{
-                files: ['scss/style.scss'],
-                tasks: ['sass']
-            },
-            css:{
-                files: ['css/style.css'],
-                tasks: ['cssmin']
-            }
-
-        },
-        sass: {
-            options:{
-                sourceMap: true
-            },
-            dist: {
-                files: {
-                    'css/style.css': 'scss/style.scss'
-                }
-            }
-        },
-        cssmin: {
-            options: {
-                shorthandCompacting: false,
-                roundingPrecision: -1
-            },
-            target: {
-                files: {
-                    'css/style.min.css': 'css/style.css'
-                }
-            }
-        },
-        'http-server': {
-            'dev': {
-                root: '.',
-                port: 8000,
-                host: "0.0.0.0",
-                showDir : true,
-                autoIndex: true,
-                ext: "html",
-                runInBackground: true
-            }
-
+module.exports = function (grunt) {
+  grunt.initConfig({
+    watch: {
+      sass: {
+        files: ['scss/style.scss'],
+        tasks: ['sass']
+      },
+      css: {
+        files: ['css/style.css'],
+        tasks: ['cssmin']
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css': 'scss/style.scss'
         }
-    });
-
-    //grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-http-server');
-    grunt.registerTask('default', ['http-server','watch']);
-
+      }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'css/style.min.css': 'css/style.css'
+        }
+      }
+    },
+    'http-server': {
+      'dev': {
+        root: '.',
+        port: 8000,
+        host: "0.0.0.0",
+        showDir: true,
+        autoIndex: true,
+        ext: "html",
+        runInBackground: true
+      }
+    }
+  });
+  //grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-http-server');
+  grunt.registerTask('default', ['http-server', 'watch']);
 };
