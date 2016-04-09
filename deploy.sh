c962bf3b6dfec40d0d1c7ad6823860eea461e0c2
Yu Chen Hou <houyuchen66@gmail.com>
2016-04-09 00:10:53 -0400

    Initial CI setup

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+grunt mustache_html
+grunt sass
+grunt prefixer
+grunt babel
+hash="Deploy commit $(git rev-parse HEAD)"
+echo "Deploying $(git rev-parse HEAD) to master"
+git add .
+git commit -m $hash
+git checkout 
+git push  --force --quiet source:master > /dev/null 2>&1
