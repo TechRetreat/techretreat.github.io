d0fa4dd6161daeaadc7d91265208a976ec31b98a
Yu Chen Hou <houyuchen66@gmail.com>
2016-04-09 00:17:33 -0400

    Another go

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,4 @@ hash="Deploy commit $(git rev-parse HEAD)"
 echo "Deploying $(git rev-parse HEAD) to master"
 git add .
 git commit -m "$hash"
-git checkout 
-git push  --force --quiet source:master > /dev/null 2>&1
+git push --force --quiet source:master > /dev/null 2>&1
