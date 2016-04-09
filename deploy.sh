94826145f20434dcc838c4789b5a75467c603fcb
Circle CI <yuchen@techretreat.ca>
2016-04-09 00:24:56 -0400

    Deploy commit 848a6c80d74ee3ba4c64f849c42151a3cbf73f30

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ hash="Deploy commit $(git rev-parse HEAD)"
 echo "Deploying $(git rev-parse HEAD) to master"
 git add .
 git commit -m "$hash"
-git push --force source:master
+git push --force origin master
