4533516cfcd2496b19d1f659c6f46ae0ca16b10b
Yu Chen Hou <houyuchen66@gmail.com>
2016-04-09 00:21:51 -0400

    Third time is the charm

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@ grunt mustache_html
 grunt sass
 grunt autoprefixer
 grunt babel
+
+git config user.name "Circle CI"
+git config user.email "yuchen@techretreat.ca"
+
 hash="Deploy commit $(git rev-parse HEAD)"
 echo "Deploying $(git rev-parse HEAD) to master"
 git add .
