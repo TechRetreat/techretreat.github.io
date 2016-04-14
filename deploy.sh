88eee00673f5a3b336b6f1d46b41ecfbbbdcd365
Dave Pagurek <dave@davepagurek.com>
2016-04-14 16:16:32 -0700

    We don't need a .gitignore on master

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ git config user.email "yuchen@techretreat.ca"
 
 hash="Deploy commit $(git rev-parse HEAD)"
 echo "Deploying $(git rev-parse HEAD) to master"
+rm .gitignore # We want all compiled assets! ALL OF THEM
 git add .
 git commit -m "$hash"
 git push --force --quiet "https://github.com/TechRetreat/techretreat.github.io.git" source:master > /dev/null 2>&1
